b3dfee2914c87c5c4c21de45c76b4b09f9b37392
Alexander Worton <12762063+appliedfunctor@users.noreply.github.com>
2023-02-11 16:36:28 +0000

    Added some Getters

--- main.go
+++ main.go
@@ -4,13 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("Starting...")
-
 	grid := NewGrid()
-	grid2 := NewGrid()
-
 	grid.Print()
-	grid2.Print()
-
 	fmt.Println("Terminating...")
 }
 
@@ -22,10 +17,30 @@ type Box struct {
 	Value [][]Digit
 }
 
+/*
+*
+TODO: Write unite tests for all GetRow and Col methods
+*/
+func (b *Box) GetRow(rowIndex int) []Digit {
+	return b.Value[rowIndex]
+}
+
+func (b *Box) GetCol(colIndex int) []Digit {
+	var col []Digit
+	for _, row := range b.Value {
+		col = append(col, row[colIndex])
+	}
+	return col
+}
+
 type Grid struct {
 	Value [][]Box
 }
 
+/*
+*
+TODO: Accept data for constructor
+*/
 func NewGrid() *Grid {
 	return &Grid{
 		Value: [][]Box{},
@@ -36,12 +51,25 @@ func (g *Grid) Print() {
 	fmt.Println("Printing")
 }
 
+/*
+*
+TODO: Write unite tests for all GetRow and Col methods
+*/
 func (g *Grid) GetRow(rowIndex int) []Digit {
 	gridIndex := rowIndex / 3
 	boxIndex := rowIndex % 3
 	var row []Digit
 	for _, box := range g.Value[gridIndex] {
-		row = append(row, box.Value[boxIndex]...)
+		row = append(row, box.GetRow(boxIndex)...)
 	}
 	return row
+}
+
+func (g *Grid) GetCol(colIndex int) []Digit {
+	boxIndex := colIndex % 3
+	var col []Digit
+	for _, rowBoxes := range g.Value {
+		col = append(col, rowBoxes[boxIndex].GetCol(boxIndex)...)
+	}
+	return col
 }
