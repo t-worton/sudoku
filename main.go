cd58df57ff4e973cc5405621dbea9d8e19c41e60
Terry Worton <tworton0@gmail.com>
2023-02-11 16:10:42 +0000

    wip

--- main.go
+++ main.go
@@ -3,6 +3,45 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Starting...")
+	fmt.Println("Starting...")
+
+	grid := NewGrid()
+	grid2 := NewGrid()
+
+	grid.Print()
+	grid2.Print()
+
 	fmt.Println("Terminating...")
+}
+
+type Digit struct {
+	Value int
+}
+
+type Box struct {
+	Value [][]Digit
+}
+
+type Grid struct {
+	Value [][]Box
+}
+
+func NewGrid() *Grid {
+	return &Grid{
+		Value: [][]Box{},
+	}
+}
+
+func (g *Grid) Print() {
+	fmt.Println("Printing")
+}
+
+func (g *Grid) GetRow(rowIndex int) []Digit {
+	gridIndex := rowIndex / 3
+	boxIndex := rowIndex % 3
+	var row []Digit
+	for _, box := range g.Value[gridIndex] {
+		row = append(row, box.Value[boxIndex]...)
+	}
+	return row
 }
